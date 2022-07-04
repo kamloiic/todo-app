30f8220b3883e347ca4164dc978d6bb43d3a69c4
loïc kameni <102739543+kamloiic@users.noreply.github.com>
2022-07-04 15:24:37 +0200

    add deploy and undeploy scripts

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,5 +1,2 @@
-cd ~/todo-app-2/kubernetes
-echo "creating postgres deployment and service"
 kubectl apply -f backend.yml 
-echo "creating postgres deplooyment, service, pvc and config map"
 kubectl apply -f postgres.yml 
---
+++ backend/undeploy.sh
@@ -0,0 +1,7 @@
+kubectl delete deployment.apps/backend
+kubectl delete service/backend
+kubectl delete deployment.apps/postgres
+kubectl delete service/postgres
+kubectl detele configmap/postgres-configmap
+kubectl delete persistentvolumeclaim/postgres-pvc  
+
